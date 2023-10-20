8af0ebcc8f87e247c8b093d46b6bfdb8e13ac539
JoaquinOrtega505 <72822153+JoaquinOrtega505@users.noreply.github.com>
2023-10-20 15:34:35 -0300

    .

--- src/universidadgrupo5/accesoADatos/Conexion.java
+++ src/universidadgrupo5/accesoADatos/Conexion.java
@@ -26,7 +26,7 @@ public class Conexion {
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
                 connection = DriverManager.getConnection(URL+DB,USUARIO,PASWORD);
-            JOptionPane.showMessageDialog(null, "Conectado");
+            //JOptionPane.showMessageDialog(null, "Conectado");
                 
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error al cargar los Driver ");
