47808af5639105bf28b69e7dcd14e64b9f301530
LaloSL <127783873+LaloSL@users.noreply.github.com>
2023-10-26 17:04:40 -0300

    .

--- src/universidadgrupo5/vistas/FormularioConsulta.java
+++ src/universidadgrupo5/vistas/FormularioConsulta.java
@@ -36,7 +36,7 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
         initComponents();
         modelo = new DefaultTableModel();
         mData = new MateriaData();  //FY
-        
+        listaM = new ArrayList<>();
         cargaMaterias();
         armarCabeceraTabla();
         borrarFilaTabla();
@@ -54,15 +54,13 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        cmat = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTabla = new javax.swing.JTable();
+        jComboBox1 = new javax.swing.JComboBox<>();
 
         jLabel1.setText("Seleccione una materia:");
 
         jLabel2.setText("Listado de Alumnos por Materia");
-
-        cmat.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         jTabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -77,6 +75,12 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
         ));
         jScrollPane1.setViewportView(jTabla);
 
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -89,8 +93,8 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jLabel1)
-                        .addGap(59, 59, 59)
-                        .addComponent(cmat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(31, 31, 31)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -104,7 +108,7 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
                 .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(cmat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -112,11 +116,16 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    //Cargar alumnos
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        borrarFilaTabla();
+        cargaMaterias();
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+    //Cargar materias
     private void cargaMaterias() { 
         //cargar al combo box        
         for (Materia item : listaM) {
-            cmat.addItem(item);
+           jComboBox1.addItem(item);
         }
     }
     
@@ -142,7 +151,7 @@ public class FormularioConsulta extends javax.swing.JInternalFrame {
         }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<Materia> cmat;
+    private javax.swing.JComboBox<Materia> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
